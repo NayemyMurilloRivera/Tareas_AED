8295991c2c14ec8d4ebd1100f0de2f51784f02ff
Programmer rabbit <154576550+NayemyMurilloRivera@users.noreply.github.com>
2026-06-04 20:12:08 -0500

    Create Arbol_Perimetro.cpp

---
+++ Arbol_Perimetro.cpp
@@ -0,0 +1,31 @@
+/*
+
+TAREA
+
+SFML
+Install SFML
+
+La idea es dibujar los algoritmos usando SFML
+De hora en adelante la tarea será dibujada.
+
+La tarea de hoy, es el ejercicio del arbol del parcial.
+
+Dibuja el arbol y pinta los nodos del contorno de ese codigo.
+Ya no imprimir, sino pintar.
+
+Segun la cantidad de numeros que le profe pide.
+Actualmente es un arbol binario.
+
+Haz un arbol binario y pintas el contorno.
+
+ESTO REVISARA LA PROXIMA SEMANA, NO MAS.
+
+Los numeros:
+VISUALGO, LA PAGINA RECOMENDADA POR GUSTAMBO PARA LOS NUEVOS ALGORITMOS.
+
+RECOMIENDA QUE HAGA QUE LAS CLASES TENGAS LOS DIBUJSO YA HECHAS, PARA QUE NO DEPENDA DEL AERBOl SI NO QUE CADA CLASE DE DIBUJO VIVA PRO SU CUENTA. Y YA, LLAMAS A LA CLASE Y GENIAL!
+
+
+
+
+*/
