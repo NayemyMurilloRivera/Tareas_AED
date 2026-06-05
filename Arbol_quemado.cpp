9b4217e8ff85987331391ca0c6fe20262226d4c9
Programmer rabbit <154576550+NayemyMurilloRivera@users.noreply.github.com>
2026-06-04 20:10:12 -0500

    Add comment block for burning tree problem description
    Added a detailed comment block explaining the problem of the burning tree and the rules for its simulation.

---
+++ Arbol_quemado.cpp
@@ -0,0 +1,64 @@
+/*
+
+Van hacer el probblema del
+ARBOL QUEMADO
+
+Vamos a poner aca algunas reglas: 
+1. El Fuego se expade hacia nodos aledaños.
+2. Cada Nodo toma el mismo tiempo para quemarse.
+3. Cada Nodo se quema solo 1 vez.
+
+
+
+Da igual si es ordenado?
+
+Como funciona:
+
+Primero vamos a suponer qeu eso son arboles.
+
+Tu escoge un objetivo, por ejemplo....
+El objetvio sea el numero 14...
+
+Entonces este nodo es el que se queamra primero, seria el 14 el primer quemada ok?
+
+Si lo haces graficamente lo pinattas de un colorcito p.
+
+El fuego se exapnde a nodos aledaño, en la 2da vuelta... 21, 24, 10
+Sigueinte vuelte:
+Para aqui no se expandem as....
+En la tercera vuelta se quema el 12 y 15
+
+Y en la sieguiente vuelta se quema el 22, 23 y el 13
+
+(OJO le porfe marca niveles en cada nodo que ya se quemo)
+
+
+
+El profe nos dira el objetivo.
+Y le tendremos que decir de que forma se irá quemando.CADA NODO SE QUEMA UNA SOLA VZ TEN CUIDADO. NO VAYAS A QUEMAR EL MISMO DE NUEVO.
+
+
+EL OTRO ARBOL DEL PROFE :
+el elige como cobjetivo el 41
+segda vuelta 2 19
+3er vuelta  el 12 
+4ta vuelta
+el 82
+5ta vuelta
+el 15 y 95
+6ta vuelta  el 21 7 y 16
+
+Ese seria el roden en el que se quema ese arbol.
+
+COMO SALIDA PUEDES IMPRIMIR LA COSA.
+O LO DIBUJAS 
+
+PERO SI PUEDES AMBOS. SERIA GENIAL.
+
+EL DIBUJO ES PARA TI PARA QUE VEAS QUE DEMONIOS ESTAS CONSTRUYENDO, EN CONCLUSION
+DIBUJAME ESE ARBOL
+TE DOY EL OBJETIVO
+Y EN EL PEOR DE LOS CASOS ME LO IMPIMES, EN EL MEOJOR LO DIBUJAS.
+
+
+*/
